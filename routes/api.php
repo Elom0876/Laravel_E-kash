f39ad685dafc83cdea0477e5a43e88aced5bdb6f
Elom0876 <pierredekadjevi@gmail.com>
2026-08-10 15:40:27 +0100

    Ajout de la ROUTE user

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\CaisseController;
 use App\Http\Controllers\Preuve_depenseController;
 use App\Http\Controllers\EntrepriseController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PosteController;
 
 
 // Routes publiques (pas besoin d'être connecté)
@@ -69,6 +70,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::middleware('role:gestionnaire,superviseur')->group(function () {
         Route::post('/entreprises', [EntrepriseController::class, 'store']);
         Route::post('/caisses', [CaisseController::class, 'store']);
+        Route::get('/entreprises', [EntrepriseController::class, 'index']);
+        Route::get('/postes/', [PosteController::class, 'index']);
     });
     Route::middleware('role:gestionnaire,superviseur')->group(function () {
 
